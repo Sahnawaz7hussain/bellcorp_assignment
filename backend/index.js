76d4a6b8b8014b3df503d9d90f5338f4ec3bb876
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2023-04-20 15:35:21 +0530

    file updated

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Server running..");
+  res.send("Server running....");
 });
 app.listen(port, async () => {
   try {

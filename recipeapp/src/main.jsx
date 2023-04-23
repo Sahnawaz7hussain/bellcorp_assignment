806db345414f448399caa533034fceb146d660bb
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2023-04-23 23:58:12 +0530

    router updated

--- recipeapp/src/main.jsx
+++ recipeapp/src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <ChakraProvider>
         <App />
       </ChakraProvider>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
